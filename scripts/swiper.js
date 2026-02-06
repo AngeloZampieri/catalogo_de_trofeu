d8f3e4c91f4fdac50b9d24a28387be037dd74968
Angelo Gabriel Fabricio Zampieri <angelo-zampieri@hotmail.com>
2026-02-06 10:27:37 -0300

    Update swiper.js
    Alteração das imagens.

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -2,114 +2,235 @@ const catalogoItens = [
     {
         nome: "Troféu modelo 01",
         precos: "15cm - R$ 11,00 | 20cm - R$ 29,00  25cm - R$ 32,00 | 30cm - R$ 37,00",
-        slides: ["./imagens/Modelo 01/Modelo 01 - Foto 0.png", "./imagens/Modelo 01/Modelo 01 - Foto 1.png", "./imagens/Modelo 01/Modelo 01 - Foto 2.png", "./imagens/Modelo 01/Modelo 01 - Foto 3.png", "./imagens/Modelo 01/Modelo 01 - Foto 4.png", "./imagens/Modelo 01/Modelo 01 - Foto 5.png", "./imagens/Modelo 01/Modelo 01 - Foto 6.png", "./imagens/Modelo 01/Modelo 01 - Foto 7.png", "./imagens/Modelo 01/Modelo 1 - Foto 08 .png", "./imagens/Modelo 01/Modelo 1 - Foto 09.png"]
+        slides: [
+            "./imagens/Modelo 01/Modelo 01 - Foto 0.jpg",
+            "./imagens/Modelo 01/Modelo 01 - Foto 1.jpg",
+            "./imagens/Modelo 01/Modelo 01 - Foto 2.jpg",
+            "./imagens/Modelo 01/Modelo 01 - Foto 3.jpg",
+            "./imagens/Modelo 01/Modelo 01 - Foto 4.jpg",
+            "./imagens/Modelo 01/Modelo 01 - Foto 5.jpg",
+            "./imagens/Modelo 01/Modelo 01 - Foto 6.jpg",
+            "./imagens/Modelo 01/Modelo 01 - Foto 7.jpg",
+            "./imagens/Modelo 01/Modelo 1 - Foto 08 .jpg",
+            "./imagens/Modelo 01/Modelo 1 - Foto 09.jpg"
+        ]
     },
     {
         nome: "Troféu modelo 02",
         precos: "15cm - R$ 12,00 | 20cm - R$ 30,00  25cm - R$ 37,00 | 30cm - R$ 42,00",
-        slides: ["./imagens/Modelo 02/Modelo 02 - foto (1).png", "./imagens/Modelo 02/Modelo 02 - foto (1).png", "./imagens/Modelo 02/Modelo 02 - foto (2).png", "./imagens/Modelo 02/Modelo 02 - foto (3).png", "./imagens/Modelo 02/Modelo 02 - foto (4).png", "./imagens/Modelo 02/Modelo 02 - foto (5).png", "./imagens/Modelo 02/Modelo 02 - foto (6).png", "./imagens/Modelo 02/Modelo 02 - foto (7).png", "./imagens/Modelo 02/Modelo 02 - foto (8).png", "./imagens/Modelo 02/Modelo 02 - foto (9).png", "./imagens/Modelo 02/Modelo 02 - foto (10).png"]
+        slides: [
+            "./imagens/Modelo 02/Modelo 02 - foto (1).jpg",
+            "./imagens/Modelo 02/Modelo 02 - foto (1).jpg",
+            "./imagens/Modelo 02/Modelo 02 - foto (2).jpg",
+            "./imagens/Modelo 02/Modelo 02 - foto (3).jpg",
+            "./imagens/Modelo 02/Modelo 02 - foto (4).jpg",
+            "./imagens/Modelo 02/Modelo 02 - foto (5).jpg",
+            "./imagens/Modelo 02/Modelo 02 - foto (6).jpg",
+            "./imagens/Modelo 02/Modelo 02 - foto (7).jpg",
+            "./imagens/Modelo 02/Modelo 02 - foto (8).jpg",
+            "./imagens/Modelo 02/Modelo 02 - foto (9).jpg",
+            "./imagens/Modelo 02/Modelo 02 - foto (10).jpg"
+        ]
     },
     {
         nome: "Troféu modelo 03",
         precos: "15cm - R$ 12,00 | 20cm - R$ 30,00  25cm - R$ 34,00 | 30cm - R$ 38,00",
-        slides: ["./imagens/Modelo 03/Modelo 03 - foto  (1).png", "./imagens/Modelo 03/Modelo 03 - foto  (2).png", "./imagens/Modelo 03/Modelo 03 - foto  (3).png", "./imagens/Modelo 03/Modelo 03 - foto  (4).png", "./imagens/Modelo 03/Modelo 03 - foto  (5).png", "./imagens/Modelo 03/Modelo 03 - foto  (6).png", "./imagens/Modelo 03/Modelo 03 - foto  (7).png", "./imagens/Modelo 03/Modelo 03 - foto  (8).png", "./imagens/Modelo 03/Modelo 03 - foto  (9).png", "./imagens/Modelo 03/Modelo 03 - foto  (10).png"]
+        slides: [
+            "./imagens/Modelo 03/Modelo 03 - foto  (1).jpg",
+            "./imagens/Modelo 03/Modelo 03 - foto  (2).jpg",
+            "./imagens/Modelo 03/Modelo 03 - foto  (3).jpg",
+            "./imagens/Modelo 03/Modelo 03 - foto  (4).jpg",
+            "./imagens/Modelo 03/Modelo 03 - foto  (5).jpg",
+            "./imagens/Modelo 03/Modelo 03 - foto  (6).jpg",
+            "./imagens/Modelo 03/Modelo 03 - foto  (7).jpg",
+            "./imagens/Modelo 03/Modelo 03 - foto  (8).jpg",
+            "./imagens/Modelo 03/Modelo 03 - foto  (9).jpg",
+            "./imagens/Modelo 03/Modelo 03 - foto  (10).jpg",
+        ]
     },
     {
         nome: "Troféu modelo 04",
         precos: "15cm - R$ 12,00 | 20cm - R$ 30,00  25cm - R$ 35,00 | 30cm - R$ 40,00",
-        slides: ["./imagens/Modelo 04/Modelo 04 - foto (1).png", "./imagens/Modelo 04/Modelo 04 - foto (2).png", "./imagens/Modelo 04/Modelo 04 - foto (3).png", "./imagens/Modelo 04/Modelo 04 - foto (4).png", "./imagens/Modelo 04/Modelo 04 - foto (5).png", "./imagens/Modelo 04/Modelo 04 - foto (6).png", "./imagens/Modelo 04/Modelo 04 - foto (7).png", "./imagens/Modelo 04/Modelo 04 - foto (8).png", "./imagens/Modelo 04/Modelo 04 - foto (9).png", "./imagens/Modelo 04/Modelo 04 - foto (10).png", "./imagens/Modelo 04/Modelo 04 - foto (11).png", "./imagens/Modelo 04/Modelo 04 - foto (12).png", "./imagens/Modelo 04/Modelo 04 - foto (13).png", "./imagens/Modelo 04/Modelo 04 - foto (14).png", "./imagens/Modelo 04/Modelo 04 - foto (15).png"]
+        slides: [
+            "./imagens/Modelo 04/Modelo 04 - foto (1).jpg",
+            "./imagens/Modelo 04/Modelo 04 - foto (2).jpg",
+            "./imagens/Modelo 04/Modelo 04 - foto (3).jpg",
+            "./imagens/Modelo 04/Modelo 04 - foto (4).jpg",
+            "./imagens/Modelo 04/Modelo 04 - foto (5).jpg",
+            "./imagens/Modelo 04/Modelo 04 - foto (6).jpg",
+            "./imagens/Modelo 04/Modelo 04 - foto (7).jpg",
+            "./imagens/Modelo 04/Modelo 04 - foto (8).jpg",
+            "./imagens/Modelo 04/Modelo 04 - foto (9).jpg",
+            "./imagens/Modelo 04/Modelo 04 - foto (10).jpg",
+            "./imagens/Modelo 04/Modelo 04 - foto (11).jpg",
+            "./imagens/Modelo 04/Modelo 04 - foto (12).jpg",
+            "./imagens/Modelo 04/Modelo 04 - foto (13).jpg",
+            "./imagens/Modelo 04/Modelo 04 - foto (14).jpg",
+            "./imagens/Modelo 04/Modelo 04 - foto (15).jpg"
+        ]
     },
     {
         nome: "Troféu modelo 05",
         precos: "15cm - R$ 12,00 | 20cm - R$ 30,00  25cm - R$ 35,00 | 30cm - R$ 40,00",
-        slides: ["./imagens/Modelo 05/Modelo 05 - foto (1).png", "./imagens/Modelo 05/Modelo 05 - foto (2).png", "./imagens/Modelo 05/Modelo 05 - foto (3).png", "./imagens/Modelo 05/Modelo 05 - foto (4).png", "./imagens/Modelo 05/Modelo 05 - foto (5).png", "./imagens/Modelo 05/Modelo 05 - foto (6).png", "./imagens/Modelo 05/Modelo 05 - foto (7).png", "./imagens/Modelo 05/Modelo 05 - foto (8).png", "./imagens/Modelo 05/Modelo 05 - foto (9).png", "./imagens/Modelo 05/Modelo 05 - foto (10).png", "./imagens/Modelo 05/Modelo 05 - foto (11).png", "./imagens/Modelo 05/Modelo 05 - foto (12).png", "./imagens/Modelo 05/Modelo 05 - foto (13).png", "./imagens/Modelo 05/Modelo 05 - foto (14).png", "./imagens/Modelo 05/Modelo 05 - foto (15).png"]
+        slides: [
+            "./imagens/Modelo 05/Modelo 05 - foto (1).jpg",
+            "./imagens/Modelo 05/Modelo 05 - foto (2).jpg",
+            "./imagens/Modelo 05/Modelo 05 - foto (3).jpg",
+            "./imagens/Modelo 05/Modelo 05 - foto (4).jpg",
+            "./imagens/Modelo 05/Modelo 05 - foto (5).jpg",
+            "./imagens/Modelo 05/Modelo 05 - foto (6).jpg",
+            "./imagens/Modelo 05/Modelo 05 - foto (7).jpg",
+            "./imagens/Modelo 05/Modelo 05 - foto (8).jpg",
+            "./imagens/Modelo 05/Modelo 05 - foto (9).jpg",
+            "./imagens/Modelo 05/Modelo 05 - foto (10).jpg",
+            "./imagens/Modelo 05/Modelo 05 - foto (11).jpg",
+            "./imagens/Modelo 05/Modelo 05 - foto (12).jpg",
+            "./imagens/Modelo 05/Modelo 05 - foto (13).jpg",
+            "./imagens/Modelo 05/Modelo 05 - foto (14).jpg",
+            "./imagens/Modelo 05/Modelo 05 - foto (15).jpg"
+        ]
     },
     {
         nome: "Troféu modelo 06",
         precos: "15cm - R$ 12,00 | 20cm - R$ 30,00  25cm - R$ 35,00 | 30cm - R$ 40,00",
-        slides: ["./imagens/Modelo 06/Modelo 06 - foto (1).png", "./imagens/Modelo 06/Modelo 06 - foto (2).png", "./imagens/Modelo 06/Modelo 06 - foto (3).png", "./imagens/Modelo 06/Modelo 06 - foto (4).png", "./imagens/Modelo 06/Modelo 06 - foto (5).png", "./imagens/Modelo 06/Modelo 06 - foto (6).png", "./imagens/Modelo 06/Modelo 06 - foto (7).png", "./imagens/Modelo 06/Modelo 06 - foto (8).png", "./imagens/Modelo 06/Modelo 06 - foto (9).png", "./imagens/Modelo 06/Modelo 06 - foto (10).png"]
+        slides: [
+            "./imagens/Modelo 06/Modelo 06 - foto (1).jpg",
+            "./imagens/Modelo 06/Modelo 06 - foto (2).jpg",
+            "./imagens/Modelo 06/Modelo 06 - foto (3).jpg",
+            "./imagens/Modelo 06/Modelo 06 - foto (4).jpg",
+            "./imagens/Modelo 06/Modelo 06 - foto (5).jpg",
+            "./imagens/Modelo 06/Modelo 06 - foto (6).jpg",
+            "./imagens/Modelo 06/Modelo 06 - foto (7).jpg",
+            "./imagens/Modelo 06/Modelo 06 - foto (8).jpg",
+            "./imagens/Modelo 06/Modelo 06 - foto (9).jpg",
+            "./imagens/Modelo 06/Modelo 06 - foto (10).jpg"
+        ]
     },
     {
         nome: "Troféu modelo 07",
         precos: "15cm - R$ 13,00 | 20cm - R$ 33,00  25cm - R$ 37,00 | 30cm - R$ 43,00",
-        slides: ["./imagens/Modelo 07/Modelo 07 - foto (1).png", "./imagens/Modelo 07/Modelo 07 - foto (2).png", "./imagens/Modelo 07/Modelo 07 - foto (3).png", "./imagens/Modelo 07/Modelo 07 - foto (4).png", "./imagens/Modelo 07/Modelo 07 - foto (5).png", "./imagens/Modelo 07/Modelo 07 - foto (6).png", "./imagens/Modelo 07/Modelo 07 - foto (7).png", "./imagens/Modelo 07/Modelo 07 - foto (8).png", "./imagens/Modelo 07/Modelo 07 - foto (9).png", "./imagens/Modelo 07/Modelo 07 - foto (10).png"]
+        slides: [
+            "./imagens/Modelo 07/Modelo 07 - foto (1).jpg",
+            "./imagens/Modelo 07/Modelo 07 - foto (2).jpg",
+            "./imagens/Modelo 07/Modelo 07 - foto (3).jpg",
+            "./imagens/Modelo 07/Modelo 07 - foto (4).jpg",
+            "./imagens/Modelo 07/Modelo 07 - foto (5).jpg",
+            "./imagens/Modelo 07/Modelo 07 - foto (6).jpg",
+            "./imagens/Modelo 07/Modelo 07 - foto (7).jpg",
+            "./imagens/Modelo 07/Modelo 07 - foto (8).jpg",
+            "./imagens/Modelo 07/Modelo 07 - foto (9).jpg",
+            "./imagens/Modelo 07/Modelo 07 - foto (10).jpg"
+        ]
     },
     {
         nome: "Troféu modelo 08",
         precos: "15cm - R$ 13,00 | 20cm - R$ 30,00  25cm - R$ 36,00 | 30cm - R$ 40,00",
-        slides: ["./imagens/Modelo 08/Modelo 08 - foto (1).png", "./imagens/Modelo 08/Modelo 08 - foto (2).png", "./imagens/Modelo 08/Modelo 08 - foto (3).png", "./imagens/Modelo 08/Modelo 08 - foto (4).png", "./imagens/Modelo 08/Modelo 08 - foto (5).png", "./imagens/Modelo 08/Modelo 08 - foto (6).png", "./imagens/Modelo 08/Modelo 08 - foto (7).png", "./imagens/Modelo 08/Modelo 08 - foto (8).png", "./imagens/Modelo 08/Modelo 08 - foto (9).png", "./imagens/Modelo 08/Modelo 08 - foto (10).png"]
+        slides: [
+            "./imagens/Modelo 08/Modelo 08 - foto (1).jpg",
+            "./imagens/Modelo 08/Modelo 08 - foto (2).jpg",
+            "./imagens/Modelo 08/Modelo 08 - foto (3).jpg",
+            "./imagens/Modelo 08/Modelo 08 - foto (4).jpg",
+            "./imagens/Modelo 08/Modelo 08 - foto (5).jpg",
+            "./imagens/Modelo 08/Modelo 08 - foto (6).jpg",
+            "./imagens/Modelo 08/Modelo 08 - foto (7).jpg",
+            "./imagens/Modelo 08/Modelo 08 - foto (8).jpg",
+            "./imagens/Modelo 08/Modelo 08 - foto (9).jpg",
+            "./imagens/Modelo 08/Modelo 08 - foto (10).jpg"
+        ]
     },
     {
         nome: "Troféu modelo 09",
         precos: "15cm - R$ 11,00 | 20cm - R$ 30,00  25cm - R$ 32,00 | 30cm - R$ 40,00",
-        slides: ["./imagens/Modelo 09/Modelo 09 - foto (1).png", "./imagens/Modelo 09/Modelo 09 - foto (2).png", "./imagens/Modelo 09/Modelo 09 - foto (3).png", "./imagens/Modelo 09/Modelo 09 - foto (4).png", "./imagens/Modelo 09/Modelo 09 - foto (5).png", "./imagens/Modelo 09/Modelo 09 - foto (6).png", "./imagens/Modelo 09/Modelo 09 - foto (7).png", "./imagens/Modelo 09/Modelo 09 - foto (8).png", "./imagens/Modelo 09/Modelo 09 - foto (9).png", "./imagens/Modelo 09/Modelo 09 - foto (10).png", "./imagens/Modelo 09/Modelo 09 - foto (11).png"]
+        slides: [
+            "./imagens/Modelo 09/Modelo 09 - foto (1).jpg",
+            "./imagens/Modelo 09/Modelo 09 - foto (2).jpg",
+            "./imagens/Modelo 09/Modelo 09 - foto (3).jpg",
+            "./imagens/Modelo 09/Modelo 09 - foto (4).jpg",
+            "./imagens/Modelo 09/Modelo 09 - foto (5).jpg",
+            "./imagens/Modelo 09/Modelo 09 - foto (6).jpg",
+            "./imagens/Modelo 09/Modelo 09 - foto (7).jpg",
+            "./imagens/Modelo 09/Modelo 09 - foto (8).jpg",
+            "./imagens/Modelo 09/Modelo 09 - foto (9).jpg",
+            "./imagens/Modelo 09/Modelo 09 - foto (10).jpg",
+            "./imagens/Modelo 09/Modelo 09 - foto (11).jpg"
+        ]
     },
     {
         nome: "Troféu modelo 10",
         precos: "15cm - R$ 13,00 | 20cm - R$ 30,00  25cm - R$ 34,00 | 30cm - R$ 39,00",
-        slides: ["./imagens/Modelo 10/Modelo 10 - foto (1).png", "./imagens/Modelo 10/Modelo 10 - foto (2).png", "./imagens/Modelo 10/Modelo 10 - foto (3).png", "./imagens/Modelo 10/Modelo 10 - foto (4).png", "./imagens/Modelo 10/Modelo 10 - foto (5).png", "./imagens/Modelo 10/Modelo 10 - foto (6).png", "./imagens/Modelo 10/Modelo 10 - foto (7).png", "./imagens/Modelo 10/Modelo 10 - foto (8).png", "./imagens/Modelo 10/Modelo 10 - foto (9).png", "./imagens/Modelo 10/Modelo 10 - foto (10).png"]
+        slides: [
+            "./imagens/Modelo 10/Modelo 10 - foto (1).jpg",
+            "./imagens/Modelo 10/Modelo 10 - foto (2).jpg",
+            "./imagens/Modelo 10/Modelo 10 - foto (3).jpg",
+            "./imagens/Modelo 10/Modelo 10 - foto (4).jpg",
+            "./imagens/Modelo 10/Modelo 10 - foto (5).jpg",
+            "./imagens/Modelo 10/Modelo 10 - foto (6).jpg",
+            "./imagens/Modelo 10/Modelo 10 - foto (7).jpg",
+            "./imagens/Modelo 10/Modelo 10 - foto (8).jpg",
+            "./imagens/Modelo 10/Modelo 10 - foto (9).jpg",
+            "./imagens/Modelo 10/Modelo 10 - foto (10).jpg"
+        ]
     }
 ];
 
 const catalogoContainer = document.querySelector('.catalogo');
-let htmlFinal = ''; // Variável para acumular todo o HTML
+let swiperInstances = [];
 
-// 1. GERAÇÃO DO HTML (OTIMIZADO)
+// 1. Geração do HTML
 catalogoItens.forEach((item, index) => {
-    
-    // Adicionei loading="lazy" nas imagens dos slides
+    // Cria os slides
     const slidesHTML = item.slides.map(src =>
-        `<div class="swiper-slide">
-            <img src="${src}" alt="${item.nome}" loading="lazy" style="width:100%; height:auto;">
-         </div>`
+        `<div class="swiper-slide"><img src="${src}" alt="${item.nome}"></div>`
     ).join('');
 
-    htmlFinal += `
+    // Cria a estrutura completa do card
+    const modeloHTML = `
         <div class="modelo-item modelo-${index + 1}">
             <div class="swiper swiper-${index + 1}"> 
+                <div class="swiper-pagination"></div>
+                
                 <div class="swiper-wrapper">
                     ${slidesHTML}
                 </div>
-                <div class="swiper-pagination"></div>
+
                 <div class="swiper-button-prev"></div>
                 <div class="swiper-button-next"></div>
             </div>
 
-            <div class="preco_container">
-                <p class="preco_texto"> 
-                    <b class="titulo_destaque">${item.nome}:</b> 
-                    <br> 
-                    <span class="modelo-precos">${item.precos}</span>
-                </p>
-            </div>
+        <div class="preco_container">
+            <p class="preco_texto"> 
+            <b class="titulo_destaque">${item.nome}:</b> 
+            <br> 
+            <span class="modelo-precos">${item.precos}</span>
+            </p>
         </div>
     `;
+
+    // Adiciona o HTML ao container do catálogo
+    catalogoContainer.insertAdjacentHTML('beforeend', modeloHTML);
 });
 
-// Inserção única no DOM (Evita recálculos de layout)
-catalogoContainer.insertAdjacentHTML('beforeend', htmlFinal);
-
-// 2. INICIALIZAÇÃO DO SWIPER (OTIMIZADO)
+// 2. Inicialização do Swiper
 catalogoItens.forEach((item, index) => {
     const swiperSelector = `.swiper-${index + 1}`;
 
-    new Swiper(swiperSelector, {
+    const newSwiper = new Swiper(swiperSelector, {
         loop: true,
         slidesPerView: 1,
-        // As propriedades abaixo melhoram muito a velocidade
-        lazy: true, 
-        observer: true,
-        observeParents: true,
-        
+
+        // Paginação
         pagination: {
             el: `${swiperSelector} .swiper-pagination`,
             type: 'bullets',
             clickable: true,
         },
 
+        // Navegação
         navigation: {
             nextEl: `${swiperSelector} .swiper-button-next`,
             prevEl: `${swiperSelector} .swiper-button-prev`,
         },
     });
+
+    swiperInstances.push(newSwiper);
 });
